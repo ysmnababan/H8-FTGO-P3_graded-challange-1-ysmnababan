54bc53141030974ff0e78eae0dbb5782e06c71b7
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-06-23 19:10:42 +0700

    feat: add data seeding for users and fields collections

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"context"
 	"graded-challange-1-ysmnababan/config"
+	"graded-challange-1-ysmnababan/helper"
+	"graded-challange-1-ysmnababan/models"
 	"graded-challange-1-ysmnababan/router"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
@@ -23,10 +26,38 @@ func main() {
 	//initialize endpoint
 	router.InitRoutes(e, db)
 
+	// initialize seed data for user and field
+	dataSeeding(db)
+
 	// get port from .env
 	err := godotenv.Load()
 	if err != nil {
 		panic(err)
 	}
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
+
+func dataSeeding(db *mongo.Database) {
+	users := []interface{}{
+		models.User{Name: "yoland", Email: "yoland@gmail.com"},
+		models.User{Name: "admin", Email: "admin@gmail.com"},
+	}
+
+	fields := []interface{}{
+		models.Field{Name: "Field 1", Price: 50.0, Status: "Available"},
+		models.Field{Name: "Field 2", Price: 60.0, Status: "Available"},
+		models.Field{Name: "Field 3", Price: 55.0, Status: "Unavailable"},
+	}
+
+	res, err := db.Collection("users").InsertMany(context.TODO(), users)
+	if err != nil {
+		helper.Logging(nil).Error(err)
+	}
+	helper.Logging(nil).Info("USER: ", res)
+
+	res, err = db.Collection("fields").InsertMany(context.TODO(), fields)
+	if err != nil {
+		helper.Logging(nil).Error(err)
+	}
+	helper.Logging(nil).Info("FIELDS: ", res)
 }
