3c487b957a3016bdca1b1b19c94849b39f05b789
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-06-23 18:56:37 +0700

    feat: add implementation for transaction controller

--- controller/transaction_controller.go
+++ controller/transaction_controller.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	"graded-challange-1-ysmnababan/helper"
+	"graded-challange-1-ysmnababan/models"
 	"graded-challange-1-ysmnababan/repository"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type TransactionController struct {
@@ -11,26 +15,92 @@ type TransactionController struct {
 }
 
 func (c *TransactionController) GetAllTransactions(e echo.Context) error {
+	res, err := c.TR.GetTransactions()
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
 
-	return nil
+	return e.JSON(http.StatusOK, map[string]interface{}{"Message:": "All transaction list", "Data": res})
 }
- 
-func (c *TransactionController) GetTransactionsByID(e echo.Context) error {
 
-	return nil
+func (c *TransactionController) GetTransactionsByID(e echo.Context) error {
+	// get id
+	t_id := e.Param("id")
+	res, err := c.TR.GetTransactionID(t_id)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
+
+	return e.JSON(http.StatusOK, map[string]interface{}{"Message:": "Transaction by a user", "Data": res})
 }
 
 func (c *TransactionController) CreateTransaction(e echo.Context) error {
+	// get body request
+	var req models.Transaction
+	err := e.Bind(&req)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
 
-	return nil
+	helper.Logging(e).Info("INPUT DATA :", req)
+
+	// validate request
+	if req.FieldID == primitive.NilObjectID || req.UserID == primitive.NilObjectID || req.EndTime == "" || req.StartTime == "" {
+		return e.JSON(http.StatusBadRequest, "error or missing param")
+	}
+
+	// validate time
+	if !helper.IsTimeValid(req.StartTime, req.EndTime) {
+		return e.JSON(http.StatusBadRequest, "time is invalid")
+	}
+
+	res, err := c.TR.CreateTransaction(&req)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
+
+	return e.JSON(http.StatusCreated, res)
 }
 
 func (c *TransactionController) EditTransaction(e echo.Context) error {
+	// get id
+	t_id := e.Param("id")
 
-	return nil
+	// get body request
+	var req models.Transaction
+	err := e.Bind(&req)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
+
+	helper.Logging(e).Info("INPUT DATA :", req)
+
+	// validate request
+	if req.FieldID == primitive.NilObjectID || req.UserID == primitive.NilObjectID || req.EndTime == "" || req.StartTime == "" {
+		return e.JSON(http.StatusBadRequest, "error or missing param")
+	}
+
+	// validate time
+	if !helper.IsTimeValid(req.StartTime, req.EndTime) {
+		return e.JSON(http.StatusBadRequest, "time is invalid")
+	}
+
+	res, err := c.TR.UpdateTransaction(t_id, &req)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
+
+	return e.JSON(http.StatusOK, res)
 }
 
 func (c *TransactionController) DeleteTransaction(e echo.Context) error {
+	// get id
+	t_id := e.Param("id")
 
-	return nil
+	res, err := c.TR.DeleteTransaction(t_id)
+	if err != nil {
+		return helper.ParseError(err, e)
+	}
+
+	return e.JSON(http.StatusOK, res)
 }
